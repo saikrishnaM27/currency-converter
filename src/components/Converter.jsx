b4e2a5892a08c67141fae700cc493d8b18d7103e
saikrishnaM27 <saikrishnamaggidi27@gmail.com>
2024-04-12 10:39:44 +0530

    commited

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -8,11 +8,7 @@ const Converter = () => {
     const [to, setTo] = useState('inr');
     const [cAmt, setCAmt] = useState(0);
     const eArr = Object.entries(countries);
-    const [fromRate, setFromRate] = useState()
-
-    const handleConvert = () => {
-
-    }
+    const [convAmt, setConvAmt] = useState(0);
 
     const API_Req = () => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from}.json`)
@@ -21,20 +17,21 @@ const Converter = () => {
             })
             .then((data) => {
                 setCAmt(data[`${from}`][`${to}`] * input)
-                console.log(data)
             })
     }
 
     useEffect(() => {
         API_Req();
-    })
+    }, [from, to, input])
 
 
-
+    const handleConvert = () => {
+        setConvAmt(cAmt);
+    }
 
     return (
 
-        <div className="converterApp w-50 m-auto border p-4">
+        <div className="converterApp w-50 m-auto border p-4 bg-white">
             <div className="inputField d-flex justify-content-evenly align-items-center mb-4">
                 <div className="amount">
                     <h3>Amount</h3>
@@ -70,12 +67,12 @@ const Converter = () => {
                     </select>
                 </div>
             </div>
-            {/* <div className="convert ">
-                <button className="btn btn-primary w-25" onClick={handleConvert}>Convert</button>
-            </div> */}
+            <div className="convert ">
+                <button className="btn btn-primary w-50" onClick={handleConvert}>Convert</button>
+            </div>
             <div className="outputField px-4 mt-4 border py-2">
                 <h3 className="">Converted Amount is:</h3>
-                <h3 className="px-4 mt-4 text-success">{cAmt}</h3>
+                <h3 className="px-4 mt-4 text-success">{convAmt}</h3>
             </div>
         </div >
     )
