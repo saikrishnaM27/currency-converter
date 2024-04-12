39b0a2bdc808519ce6a55e1a2bd05d02da2d108f
saikrishnaM27 <saikrishnamaggidi27@gmail.com>
2024-04-12 11:05:03 +0530

    commited

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 const Navigation = () => {
     return (
         <>
-            <div className="text-center shadow-sm p-2 mb-3  ">
+            <div className="text-center shadow-sm p-2 mb-3 bg-white">
                 <div id="title">
                     <h2><u>Currency Conveter</u></h2>
                 </div>
